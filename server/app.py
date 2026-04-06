0726306f4060224bc128f80b1ab96d73c33e8fb9
C Vikas Raju <vikasrajuc15@gmail.com>
2026-04-06 23:41:46 +0530

    Update of Entry point main()

--- server/app.py
+++ server/app.py
@@ -69,6 +69,10 @@ def get_spec():
         return f.read()
 
 
-if __name__ == "__main__":
+def main():
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
